dcf61e8647778cde445526b66339cd2a15a2cf9d
blk_addr <blk_addr@yahoo.com>
2024-12-01 07:19:35 -0600

    Updated memory values, added Rundeck and TODOs

--- environments/local/main.tf
+++ environments/local/main.tf
@@ -8,7 +8,17 @@ provider "proxmox" {
 
 # Sets up the IaC server on Proxmox
 module "iac-host" {
-  source = "../../modules/ubuntu-template"
-  vm_name = "iac-host"
+  source   = "../../modules/ubuntu-template"
+  vm_name  = "iac-host"
+  memory   = 8192
   snippets = [ "user=local:snippets/default-user.yml", "vendor=local:snippets/iac-tools.yml" ]
 }
+
+# TODO: Set up Proxmox TLS
+#   - https://pve.proxmox.com/wiki/Certificate_Management
+# TODO: set hostname in hostname file
+# TODO: get rundeck behind a proxy with TLS 
+#   - https://blog.walnuthomelab.com/posts/2022/june/024-rundeck-install/
+#   - https://docs.nginx.com/nginx/admin-guide/installing-nginx/installing-nginx-open-source/#installing-a-prebuilt-debian-package-from-the-official-nginx-repository
+# TODO: put snippets in this repo, upload to PVE
+#   - https://stackoverflow.com/questions/78628828/unable-to-upload-cloudinit-snippets-via-ssh
